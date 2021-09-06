98bae60aa9857d8be18ec7b987e95f64c1b521ab
Adilalaminfo <mdadilalaminfo@gmail.com>
2021-09-06 12:42:45 +0530

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-function fun(){
-for(let i=1;i<=5;i++){
-    setTimeout(function (){
-        console.log(i);
-    },i*1000);
-}
+//function statement
+function a(){
     console.log("Md Adil Alam");
 }
-fun();
+a();
+//function Expression
+var b=function(){
+    console.log("function Expression");
+}
+b();
+//Anonymous function without function is called anonymous function
+
+var c=function (){
+    console.log("Anonymous function");
+
+}
+c();
+//Named function Expression
+var d=function xyz(){
+    console.log(xyz);
+}
+d();
+//Difference between Parameter & Argument
+var e=function (param1,param2){ //Parameter
+    console.log("Diffrence between Parameter and Argument");
+}
+e(1,3); //Argument
+
+//First class Function
+ var f=function a(param1){
+   return function abc(){
+
+   }
+  }
+  console.log(f());
+   
